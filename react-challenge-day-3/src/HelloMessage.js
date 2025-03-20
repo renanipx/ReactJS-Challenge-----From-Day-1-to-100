acb8e9a67845b87908a93b98821f8a450d6cae43
renanipx <137443957+renanipx@users.noreply.github.com>
2025-03-20 11:31:39 -0300

    Create a functional component and render it fix: import missing HelloMessage component in index.js
    - Added missing import for the HelloMessage component in index.js
    - Ensured HelloMessage is rendered along with the App component

---
+++ react-challenge-day-3/src/HelloMessage.js
@@ -0,0 +1,13 @@
+// HelloMessage.js
+import React from 'react';
+
+const HelloMessage = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a functional React component.</p>
+    </div>
+  );
+};
+
+export default HelloMessage;
