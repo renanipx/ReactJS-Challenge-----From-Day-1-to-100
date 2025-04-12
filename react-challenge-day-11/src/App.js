814ef4506f786cb44afe6094c071c640455a619e
renanipx <137443957+renanipx@users.noreply.github.com>
2025-04-12 15:17:45 -0300

    feat: add support for React fragments to group multiple elements

---
+++ react-challenge-day-11/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import List from './List';
+import TestComponent from './TestComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+
+        <TestComponent />
+        <List />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-challenge-day-11/src/TestComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TestComponent() {
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <p>This is a paragraph inside a fragment.</p>
+    </>
+  );
+}
+
+export default TestComponent;
